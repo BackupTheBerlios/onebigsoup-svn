90e31b922a8ffa57c100618c0ab0f6f22925cd3d
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2004-07-07 04:02:30 +0000

    Renamed functions, ULI/HTTP now properly returns result string, and a new 'prompt' function, for convenient interaction with an ULI interface.

--- src/uli/uli.py
+++ src/uli/uli.py
@@ -1,7 +1,22 @@
 import urllib, xmlrpclib
 
-def uli_http( url, line ):
-    return urllib.urlopen( url, urllib.urlencode( {"ULI":line} ) )
+def http( url, line ):
+    return urllib.urlopen( url, urllib.urlencode( {"ULI":line} ) ).read()
 
-def uli_xmlrpc( url, line ):
+def xmlrpc( url, line ):
     return xmlrpclib.ServerProxy( url ).uli( line )
+
+def prompt( prompt_string, uli_url, call_func ):
+    """
+    prompt_string: ex: "<uli-9095> "
+
+    uli_url:       ex: "http://services.taoriver.net:9095/"
+
+    call_func:     uli.http or uli.xmlrpc
+    """
+    input = ""
+
+    while input.upper() != "QUIT":
+        input = raw_input( prompt_string )
+        if input.upper() != "QUIT":
+            print call_func( uli_url, input )
