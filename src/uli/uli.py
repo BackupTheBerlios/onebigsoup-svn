ea8e5de3b65099435651bf5258a3ad691bb9905e
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2004-07-07 02:02:09 +0000

    Actually return the data that was retrieved.

--- src/uli/uli.py
+++ src/uli/uli.py
@@ -1,7 +1,7 @@
 import urllib, xmlrpclib
 
 def uli_http( url, line ):
-    urllib.urlopen( url, urllib.urlencode( {"ULI":line} ) )
+    return urllib.urlopen( url, urllib.urlencode( {"ULI":line} ) )
 
 def uli_xmlrpc( url, line ):
-    xmlrpclib.ServerProxy( url ).uli( line )
+    return xmlrpclib.ServerProxy( url ).uli( line )
