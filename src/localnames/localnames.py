b87dd71e09acc529f539f92fff58419aeab05b99
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2004-07-14 02:56:16 +0000

    Case insensitivity implemented.

--- src/localnames/localnames.py
+++ src/localnames/localnames.py
@@ -58,34 +58,179 @@ class NameSpaceStore:
         s.namespaces = marshal.load( open( marshal_filename, "rb" ) )
 
 
+class Link:
+    """
+    Keeps information about a particular link.
+
+    Remembers the link's URL, the link's canonical name,
+    and a list of names that bind to the link.
+
+    The first name listed is the "canonical" name.
+
+    PROMISES:
+    Do not manipulate this, except by way of a LinkStore.
+    However, feel free to query it.
+    """
+    def __init__( s, url, names=None ):
+        """
+        url: string, the URL that the Link describes
+        names: a single name (string), or a list of names
+
+        The first name bound is the "canonical" name.
+        """
+        s.url = url
+        s.names = []
+        if type(names)==type("string"):
+            s.names.append(names)
+        elif type(names)==type(["list","of","names"]):
+            s.names.extend(names)
+        else:
+            raise "Type of 'names' must be either string or list of strings."
+    def get_url(s):
+        """
+        Return URL associated with link.
+        """
+        return s.url
+    def remove_name(s,name):
+        """
+        Remove a name from the Link's list of names,
+        if it is present.
+        """
+        try:
+            s.names.remove(name)
+        except ValueError, e:
+            pass # name already in list
+    def add_name(s, name):
+        """
+        name: another name to call the Link by
+        
+        Checks to make sure, first, whether it's already there or not.
+        """
+        if name not in s.names:
+            s.names.append( name )
+    def get_names(s):
+        """
+        Returns a tuple of all names binding to the URL.
+        """
+        return tuple(s.names)
+    def get_canonical_name(s):
+        """
+        Returns the canonical name, if one was assigned,
+        or returns the first name in the internal names list.
+
+        Returns None if no names have been assigned.
+        """
+        if len(s.names)>0:
+            return s.names[0]
+        else:
+            return None
+
+
+class LinkStore:
+    """
+    Keeps a collection of named URLs.
+
+    Two indexes are maintained:
+    * upper-case name --> Link
+    * URL --> Link
+
+    Links have canonical names, so you can
+    get the canonical name as well.
+    """
+    def __init__(s):
+        s.names = {}
+        s.urls = {}
+    def bind(s, name,url):
+        """
+        INTERNAL METHOD
+
+        Binds names to URLs, maintaining consistency with two indexes:
+        * upper case name --> Link object
+        * URL --> Link object
+        """
+        prior_link = s.names.get( name.upper() )
+        if prior_link != None:
+            prior_link.remove_name( name )
+        link = s.urls.get( url )
+        if link != None:
+            link.add_name( name )
+            s.names[ name.upper() ] = link
+        else:
+            link = Link( url, name )
+            s.names[ name.upper() ] = link
+            s.urls[ url ] = link
+    def lookup(s, name):
+        """
+        Given a name, look up it's URL.
+        Case insensitive.
+        """
+        link = s.names.get(name.upper())
+        if link:
+            return link.get_url()
+        else:
+            return None
+    def canonical_names(s):
+        results=[]
+        for link in s.urls.values():
+            results.append(link.get_canonical_name())
+        results = [x for x in results if x != None]
+        return results
+    def canonical_name_for_name(s, name):
+        link = s.names[name.upper()]
+        return link.get_canonical_name()
+    def canonical_name_for_url(s, url):
+        link = s.urls[url]
+        return link.get_canonical_name()
+
 class NameSpace:
+    """
+    A NameSpace keeps information about a LocalNames name space.
+
+    This information includes:
+     * URL of the name space
+     * names table (name-URL bindings)
+     * name-Link and URL-Link indexes over the names table
+     * neighboring spaces table
+     * name-Link and URL-Link indexes over the neighboring spaces table
+     * list of neighboring spaces, for defaulting
+     * last resort name pattern, for when a name space exhausts
+       all other ways of binding names to URLs
+     * key-value metadata about the name space
+     * record of when the name space was loaded
+    """
     def __init__(s, ns_url):
         """
+        ns_url: URL of the name space description
+        
         Loads NameSpace data, automatically.
 
         ns_url:
            URL of NameSpace data to load in.
         """
         s.ns_url = ns_url
-        s.names = {}
-        s.spaces = {}
-        s.key_values = {}
-        s.defaults = []
-        s.last_resort_name_pattern = None
+
+        s.names = LinkStore() # names table
+        s.spaces = LinkStore() # spaces table
+        s.defaults = [] # list of names, in any form
+        
+        s.key_values = {} # metadata about the name space
+        
+        s.last_resort_name_pattern = None # if name can't be resolved...
+        
         s.loaded_time = None # filled with time.time() after parse
+        
         s._parse_from_url()
         
     def lookup_name(s, name):
         """
         returns resolved URL
         """
-        return s.names.get( name )
+        return s.names.lookup( name )
     def lookup_space(s, name):
         """
-        returns resolved URL of gateway
+        returns resolved URL of name space description
         """
-        return s.spaces.get( name )
-
+        return s.spaces.lookup( name )
     def default_for_name(s, name):
         """
         returns a name, made to fit the default page pattern
@@ -93,16 +238,37 @@ class NameSpace:
         if s.last_resort_name_pattern == None:
             return None
         return s.last_resort_name_pattern.replace( "$NAME", name )
-
+    def bind_name_to_url(s, name, url):
+        """
+        Bind a Name to a URL.
+        """
+        s.names.bind( name, url )
+    def bind_namespace_to_url(s, name, url ):
+        """
+        Same as bind_name_to_url, but for names
+        of other name spaces.
+        """
+        s.spaces.bind( name, url )
     def _parse_from_url(s):
+        """
+        This internal function parses the page at the URL
+        and fills out the NameSpace based on the found page.
+
+        Raises NAMESPACE_FILE_DOESNT_LOAD if URL isn't found.
+
+        After parsing out the description, NameSpace is time stamped.
+        """
         parser = lnparser.Parser( LocalNamesSink(s) )
         try:
-            url_data = urllib.urlopen( s.ns_url ).read()
+            parser.feedUrl( s.ns_url )
         except IOError,e:
             raise NAMESPACE_FILE_DOESNT_LOAD
-        parser.feedString( url_data )
         parser.parse()
         s.loaded_time = time.time()
+    def canonical_names(s):
+        return s.names.canonical_names()
+    def canonical_names_of_spaces(s):
+        return s.spaces.canonical_names()
 
 class LocalNamesSink:
     """
@@ -115,10 +281,10 @@ class LocalNamesSink:
         self.namespace.key_values[key] = value
 
     def map(self, name, uri):
-        self.namespace.names[ name ] = uri
+        self.namespace.bind_name_to_url( name,uri )
 
     def otherNameSpace( self, name, uri ):
-        self.namespace.spaces[ name ] = uri
+        self.namespace.bind_namespace_to_url( name, uri )
 
     def defaultNameSpaces(self, name):
         self.namespace.defaults.append( name )
@@ -140,7 +306,6 @@ class SpaceTraverser:
     def __init__( s, start_ns_url, store ):
         s.store = store
         s.ns = s.store.get( start_ns_url )
-
     def lookup_name( s, name ):
         "Wrapper function around NameSpace visiting"
         return s.ns.lookup_name( name )
@@ -207,9 +372,14 @@ class SpaceTraverser:
     
 class Resolver:
     """
-    Facade over the whole system.
+    Facade, simulating being based over a particular namespace url.
     """
     def __init__( s, default_ns_url, store_filename, store=None ):
+        """
+        default_ns_url: URL of namespace to start from
+        store_filename: for save & load functions
+        store: (optional) pre-existing store to use
+        """
         s.store = store
         s.store_filename = store_filename
         s.default_ns_url = default_ns_url
@@ -221,7 +391,7 @@ class Resolver:
             except IOError:
                 # file not found? it's okay.
                 pass
-        
+    
     def save(s):
         s.store.save(s.store_filename)
 
