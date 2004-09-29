407335564299efa834ab00e83e8ed0c9fd9b31a2
kpreid <kpreid@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2004-09-29 06:46:57 +0000

    HTTP GET support; {name: func} and {func: name} dictionaries

--- src/uli/uli.py
+++ src/uli/uli.py
@@ -1,17 +1,53 @@
+__all__ = ['http_post', 'http_get', 'http', 'xmlrpc', 'forms_by_name', 'form_names', 'prompt']
+
 import urllib, xmlrpclib
 
-def http( url, line ):
-   # urllib.urlopen ignores HTTP errors and doesn't tell us about them
-   uo = urllib.URLopener()
-   response = uo.open( url, urllib.urlencode( {"ULI":line} ) )
+# urllib.urlopen ignores HTTP errors and doesn't tell us about them, so we use urllib.URLopener which is less smart.
+
+# I hear that urllib2 would be better.
+
+def _check_http_response( response ):
    ctype = response.info()["Content-Type"]
-   if ctype == "text/plain":
+   if ctype.split(";")[0] == "text/plain":
       return response.read()
    else:
       raise IOError, "Wrong Content-Type for ULI response: %s" % ctype
 
+def http_post( url, line ):
+   uo = urllib.URLopener()
+   response = uo.open( url, urllib.urlencode( {"ULI":line} ) )
+   return _check_http_response(response)
+
+# for backwards compatibility
+http = http_post
+
+def http_get( urlPrefix, line ):
+   uo = urllib.URLopener()
+   response = uo.open( urlPrefix + urllib.quote(line, "") )
+   ctype = response.info()["Content-Type"]
+   return _check_http_response(response)
+
 def xmlrpc( url, line ):
     return xmlrpclib.ServerProxy( url ).uli( line )
+
+forms_by_name = { 
+  "http":      http_get,
+  "http_get":  http_get,
+  "http-get":  http_get,
+  "get":       http_get,
+  "http_post": http_post,
+  "http-post": http_post,
+  "post":      http_post,
+  "xmlrpc":    xmlrpc,
+  "xml-rpc":   xmlrpc,
+  "xml_rpc":   xmlrpc,
+}
+
+form_names = {
+  http_get:  "http_get",
+  http_post: "http_post",
+  xmlrpc:    "xmlrpc",
+}
 
 def prompt( prompt_string, uli_url, call_func ):
     """
