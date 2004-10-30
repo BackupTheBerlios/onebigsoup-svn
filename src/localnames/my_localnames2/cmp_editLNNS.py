d84211cec5b6d93ec0ac8d0a17c9ce85af60b9e9
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2004-10-30 10:23:16 +0000

    added EXTRA names, namespaces, so you can use nice defaults

--- src/localnames/my_localnames2/cmp_editLNNS.py
+++ src/localnames/my_localnames2/cmp_editLNNS.py
@@ -8,6 +8,7 @@ LN_or_NS = "unconfigured" # invoking module sets to "LN" or "NS"
 t = Template( file="cmp_editLNNS.html" )
 
 space_file=code_old.hub.namespace_files.get_space( code_info.space().upper() )
+extra_file=code_old.hub.namespace_files.get_space( "EXTRA" )
 qd = code_info.query_dict()
 
 
@@ -50,6 +51,25 @@ def register_new_names():
         code_old.hub.event_namesadded( code_info.space(), names(), url() )
         space_file.save()
 
+def register_checked_names():
+    for key in code_info.form.keys():
+        if key.startswith( "EXTRA-" ):
+            name=key[ 6: ]
+            if LN_or_NS == "LN":
+                if name == "this":
+                    url = "http://to-be-coded"
+                elif name == "edit":
+                    url = "http://to-be-coded"
+                else:
+                    url=extra_file.get_names()[name]
+                space_file.add_name( name, url )
+            elif LN_or_NS == "NS":
+                url=extra_file.get_namespaces()[name]
+                space_file.add_namespace( name, url )
+            else:
+                raise "this should never happen - note: afkpes"
+            space_file.save()
+
 def gval( k,i ):
     if code_info.form.has_key( k+str(i) ):
         return code_info.form[ k+str(i) ].value
@@ -77,6 +97,7 @@ def register_changes_deletes_reorders():
 
 def response():
     register_new_names()
+    register_checked_names()
     if code_info.form.has_key( "NUM_ENTRIES0" ):
         register_changes_deletes_reorders()
 
@@ -99,6 +120,18 @@ def response():
                           "NAME": name,
                           "URL": url, } )
     t.NUM_ENTRIES = len( t.NAMES )
+
+    if LN_or_NS == "LN":
+        extras_in_order = extra_file.get_names_in_order()
+        extras_in_order.insert( 0, ( "this", "http://to-be-coded" ) )
+        extras_in_order.insert( 0, ( "edit", "http://to-be-coded" ) )
+    elif LN_or_NS == "NS":
+        extras_in_order = extra_file.get_namespaces_in_order()
+
+    t.EXTRA = [ { "NAME": entry[0],
+                  "URL": entry[1] }
+                for entry in extras_in_order
+                if entry[0] not in [x[0] for x in in_order] ]
 
     return str(t)
 
