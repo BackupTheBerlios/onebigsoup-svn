0c02d8bcd02630ad3e55f7a3a0de84b848e4c29f
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2004-12-29 20:45:48 +0000

    leo: fresh new code, can retrieve namespaces, can perform lookups

---
+++ src/localnames/core1.1/lnparser.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python2.3
+"""
+Local Names 1.1 Namespace Description Parser
+"""
+import re
+import urllib
+
+
+ws_line_re = re.compile(r'^\s*$')
+record_type_re = re.compile(r'[.]|LN|NS|X|PATTERN', re.IGNORECASE)
+column_re = re.compile(r'\s+("(?:(:?(?![\\]")[^"]|[\\]")+)"|(?:[^\s]+))', re.UNICODE)
+
+
+class LocalNamesSyntaxError(Exception):
+    def __init__(self, msg):
+        self.msg=msg
+        Exception.__init__(self, 'Local Names v1.1 Syntax Error: %s" % msg')
+
+
+def parse_text(text):
+    """
+    Parse Local Names v1.1 description, and return results and errors.
+
+    Pass UTF-8 text to be parsed. The function returns a tuple with
+    the following form:
+
+    (results, errors)
+
+    Results have the form: (line#, "record type", "second column", "third column")
+    Errors have the form: (line#, "human readable error string")
+    
+    """
+    
+    def blank(line):
+        return ws_line_re.match(line) != None
+
+    def comment(line):
+        return len(line) > 0 and line[0] == "#"
+
+    def parse_record_type(cursor, line, parsed):
+        mo=record_type_re.match(line[cursor:])
+        if mo == None:
+            raise LocalNamesSyntaxError("unrecognized v1.1 record type- require LN, NS, X, or PATTERN")
+        parsed.append(mo.group())
+        return cursor+mo.end()
+
+    def parse_column(cursor, line, parsed, col_num):
+        mo=column_re.match(line[cursor:])
+        if mo == None:
+            raise LocalNamesSyntaxError("poorly formatted column #%d" % col_num)
+        text=mo.group(1)
+        if text[0] == u'"':
+            text = text[1:-1]
+        parsed.append(text)
+        return cursor+mo.end()
+
+    errors = []
+    results = []
+    
+    lines = text.splitlines()
+    nlines = zip(range(len(lines)), lines)
+    nlines = [(num,line) for (num,line) in nlines if not blank(line)]
+    nlines = [(num,line) for (num,line) in nlines if not comment(line)]
+
+    for num,line in nlines:
+        try:
+            parsed=[]
+            cursor=0
+            cursor=parse_record_type(cursor,line,parsed)
+            cursor=parse_column(cursor,line,parsed,2)
+            cursor=parse_column(cursor,line,parsed,3)
+            for index in [0,1,2]:
+                if parsed[index] == ".":
+                    try:
+                        parsed[index] = results[-1][index+1]
+                    except IndexError:
+                        errors.append((num, "no previous column"))
+            results.append((num,parsed[0],parsed[1],parsed[2]))
+        except LocalNamesSyntaxError, exc:
+            errors.append((num, exc.msg))
+    
+    return results, errors
+
+
+test_string = u"""
+# test string for parser
+
+X version 1.1
+
+LN "Lion Kimbro" http://www.speakeasy.org/~lion/
+LN "with \\"quotes\\" inside" http://google.com/
+. . .
+
+this line is an error
+"""
+
+if __name__ == '__main__':
+    import pprint
+    for num, line in enumerate(test_string.splitlines()):
+        print "#%2d:  %s" % (num, line)
+    pprint.pprint(parse_text(test_string))
+    
---
+++ src/localnames/core1.1/localnames.py
@@ -0,0 +1,268 @@
+"""
+Local Names 1.1 Server
+"""
+import time
+import urllib
+import re
+import sets
+
+import lnparser
+
+
+store = {}
+time_to_live = 24*60*60  # one day
+
+punctuation_re = re.compile(r'[^A-Za-z0-9\s]+', re.UNICODE)
+ws_re = re.compile(r'\s+', re.UNICODE)
+the_re = re.compile(r'\b(?:the|a|an|in|for)\b', re.UNICODE | re.IGNORECASE)
+
+loose_flags = sets.ImmutableSet(["no-case", "no-punctuation",
+                                  "no-space", "forgive-spelling"])
+
+
+def get_namespace(url):
+    """
+    Returns a namespace dictionary.
+
+    Returns a cached version if possible,
+    retrieves fresh from the web otherwise.
+
+    The namespace dictionary is structured so:
+
+    "TIME": (INTERNAL) cache timestamp
+    "TEXT": raw UTF-8 text cached
+    "ERRORS": list of parse errors;
+              each error is a tuple:
+              (line#,"error msg")
+    "LN-raw": raw list of parsed LN entries (line#,"2nd-col", "3rd-col")
+    "NS-raw": (same) 
+    "PATTERN-raw": (same)
+    "X-raw": (same)
+    "LN": dictionary of LN entries {"name":"URL"}
+    "NS": (same)
+    "PATTERN": (same)
+    "X": dictionary of X entries, {"name":["one","two",...]}
+
+    """
+    now = time.time()
+    try:
+        if now < store[url]["TIME"] + time_to_live:
+            return store[url]
+    except KeyError:
+        pass
+    
+    namespace = {}
+    namespace["TIME"] = now
+    namespace["TEXT"] = urllib.urlopen( url ).read().decode("utf-8","replace")
+    lines = namespace["TEXT"].splitlines()
+
+    (results,errors) = lnparser.parse_text(namespace["TEXT"])
+
+    namespace["ERRORS"] = errors
+    namespace["LN-raw"] = []
+    namespace["NS-raw"] = []
+    namespace["PATTERN-raw"] = []
+    namespace["X-raw"] = []
+    for line_number, record_type, second, third in results:
+        namespace[record_type+"-raw"].append((line_number, second, third))
+
+    namespace["LN"] = {}
+    namespace["NS"] = {}
+    namespace["PATTERN"] = {}
+    namespace["X"] = {}
+    for record_type in ["LN","NS","PATTERN"]:
+        for line_number, second, third in namespace[record_type+"-raw"]:
+            if not namespace[record_type].has_key(second):
+                namespace[record_type][second]=third
+            else:
+                msg = "redefinition of %s" % second
+                namespace["ERRORS"].append((line_number, msg))
+
+    store[url] = namespace
+
+    return namespace
+
+
+def lookup(path, url, flags):
+    """
+    Resolve a path to a URL.
+
+    Returns the resolved URL.
+
+    path: ["space", "space", ..., "name"]
+    url: "http://url.to.first.namespace.description/"
+    flags: Python 2.3 Set of flags
+
+    The following flags are understood:
+
+      "no-case":  ignore capitalization
+      "no-punctuation":  ignore punctuation
+      "no-space":  ignore white space
+      "forgive-spelling":  accept plausible mis-spellings
+      "loose": "no-case" + "no-punctuation" + "no-space"
+               + "check-neighboring-spaces" +
+      "check-neighboring-spaces":  check spaces linked by NS
+      "reverse":  reverse lookup (URL, not name, at end of path)
+      "NS":  namespace-lookup; (NS, not LN, at end of path)
+
+    """
+    if "loose" in flags:
+        flags = flags | loose_flags
+        flags.discard("loose")
+    assert len(path) > 0
+    namespace = get_namespace(url)
+
+    if len(path) == 1:
+        if "NS" in flags:
+            records = namespace["NS-raw"]
+        else:
+            records = namespace["LN-raw"]
+
+        # If "loose" flags are being used, try resolving without loose
+        # flags, first.  If there is an exact match, it has priority.
+
+        if flags & loose_flags:
+            result = _check_list(path[0], records, flags - loose_flags)
+            if result:
+                return result
+
+        result = _check_list(path[0], records, flags)
+        if result:
+            return result
+
+        if "check-neighboring-spaces" in flags:
+            wo_neighboring = flags.copy()
+            wo_neighboring.discard("check-neighboring-spaces")
+            urls = sets.Set()
+            for num, neighborname, url in namespace["NS-raw"]:
+                if url not in urls:
+                    result = lookup(path[0], url, wo_neighboring)
+                    if result:
+                        return result
+                    urls.add(url)
+
+        if ("FINAL" in namespace["X"]) and ("reverse" not in flags):
+            return namespace["X"][0].replace("$PAGE", path[0])
+        return None
+    else:
+        pattern_raw = namespace["PATTERN-raw"]
+        ns_raw = namespace["NS-raw"]
+        original_flags = flags
+        flags = flags.copy()
+        flags.discard("reverse")
+        if flags & loose_flags:
+            result = _check_list(path[0], pattern_raw, flags - loose_flags)
+            if result:
+                return result.replace("$PAGE", path[1])
+            result = _check_list(path[0], ns_raw, flags - loose_flags)
+            if result:
+                return lookup(path[1:], result, original_flags)
+
+        result = _check_list(path[0], pattern_raw, flags)
+        if result:
+            return result.replace("$PATH", path[1])
+        result = _check_list(path[0], ns_raw, flags)
+        if result:
+            return lookup(path[1:], result, original_flags)
+
+        return None
+
+
+def _check_list(name, records, flags):
+    """
+    Lookup a name in one of the namespace's raw lists.
+
+    Returns None if no matches found.
+    Returns the first match found, otherwise.
+    "reverse" searches return a list of matches.
+
+    name:  name to look for
+    records:  list of (line#, name, URL) to search
+    flags:  list of strings representing flags
+
+    Recognized flags: "no-case", "no-punctuation", "no-space",
+                      "forgive-spelling", "reverse"
+
+    """
+    pname = _prepare_key(name, flags)
+    reversed = "reverse" in flags
+    forgive_spelling = "forgive-spelling" in flags
+    reversed_list = []
+
+    for num, second, third in records:
+        if reversed:
+            second, third = third, second
+        psecond = _prepare_key(second, flags)
+        if pname == psecond:
+            if reversed:
+                reversed_list.append( third )
+            else:
+                return third
+        if forgive_spelling:
+            pass  # not coded yclass HostNotFound(Exception):
+
+    if reversed:
+        if len(reversed_list) > 0:
+            return reversed_list
+    return None
+
+
+def _prepare_key(name, flags):
+    """
+    Prepares a key for comparison.
+
+    name: string,  flags: list of strings
+    Recognized flags: "no-case", "no-punctuation",
+                      "no-space", "forgive-spelling".
+    """
+    if "no-case" in flags:
+        name = name.lower()
+    if "no-punctuation" in flags:
+        name = punctuation_re.sub("", name)
+    if "no-space" in flags:
+        name = ws_re.sub("", name)
+    if "forgive-spelling" in flags:
+        try:
+            if name[-1] == "s":
+                name=name[:-1]
+            if name[-3:] == "ing":
+                name=name[:-3]
+            the_re.sub("",name)
+        except IndexError:
+            pass
+    return name
+
+
+def display_namespace_text(namespace):
+    """Write namespace's cached text to the console."""
+
+    lines = namespace["TEXT"].splitlines()
+    for num, line in enumerate(lines):
+        print "#%3d:  %s" % (num, line.encode("ascii","replace"))
+
+
+def display_namespace_errors(namespace):
+    """Write namespace's cached errors to the console."""
+    lines = namespace["TEXT"].splitlines()
+    for num, msg in namespace["ERRORS"]:
+        print "  %3d:  %s" % (num, msg.encode("ascii","replace"))
+        print "        %s" % lines[num].encode("ascii","replace")
+
+
+if __name__ == '__main__':
+    url = "http://ln.taoriver.net/localnames.txt"
+    namespace = get_namespace(url)
+    display_namespace_text(namespace)
+    print
+    print "ERRORS:"
+    display_namespace_errors(namespace)
+    print "LOOKUPS:"
+    flags = sets.Set()
+    print "Wiki Wrapper:", lookup(["Wiki Wrapper"], url, flags)
+    flags.add("reverse")
+    print "reverse:",
+    print lookup(["http://wrap.localnames.taoriver.net/"], url, flags)
+    print "loose (wiki-wrappers):",
+    flags.add("loose")
+    flags.discard("reverse")
+    print lookup(["wiki-wrappers"], url, flags)
