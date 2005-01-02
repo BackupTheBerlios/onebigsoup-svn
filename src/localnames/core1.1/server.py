5c8c088be904ad93a7f5aa8a3008703e7bcc7178
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2005-01-02 00:44:55 +0000

    Loads of XML-RPC functionality! Checkpointing, about to add more.

--- src/localnames/core1.1/server.py
+++ src/localnames/core1.1/server.py
@@ -8,20 +8,46 @@ import BaseHTTPServer
 import cgi
 import xmlrpclib
 import xml.parsers.expat
+import sets
+import time
 
 import localnames
 
 
+redir_lookup_flags = sets.ImmutableSet(["loose", "check-neighboring-spaces"])
+
+
 class LocalNamesHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
-    """
-    Handles GET, POST, and interprets ULI, GTF, XML-RPC.
-    """
+    """Handles GET, POST, and interprets ULI, GTF, XML-RPC."""
     
     def do_GET(self):
-        self.send_response(301)
-        self.send_header("Location", "http://slashdot.org/")
+        if self.path.startswith("/?"):
+            args = cgi.parse_qs( self.path[2:] )
+        else:
+            args = {}
+
+        if args.get("action") == ["redir"] and \
+           args.get("namespace") != None and \
+           (args.get("lookup") != None or args.get("path") != None):
+            if args.has_key("path") and not args.has_key("lookup"):
+                args["lookup"] = args["path"].split("/")
+            url = localnames.lookup(args["lookup"],
+                                    args["namespace"][0],
+                                    redir_lookup_flags)
+            self.send_response(301)
+            self.send_header("Location", url)
+            self.end_headers()
+            return
+        
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
         self.end_headers()
+        
+        self.wfile.write('<html>'
+                         '<head><title>Local Names Server 1.1</title></head>'
+                         '<body><p>Local Names Server 1.1</p></body>'
+                         '</html>')
 
     def do_POST(self):
         clen = int(self.headers.getheader("content-length"))
@@ -33,17 +59,40 @@ class LocalNamesHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         # d = cgi.parse_qs(body) -- for later
         # -- for now, assuming always XML-RPC
         (args, funct) = call_data
-        if funct == "filterData":
-            result = self.filterData(args[0], args[1], args[2])
+        xmlrpc_bindings = {"filterData": self.xmlrpc_filterData,
+                           "cached": self.xmlrpc_cached,
+                           "dump_cache": self.xmlrpc_dump_cache,
+                           "validate": self.xmlrpc_validate,
+                           "wiki.filterData": self.xmlrpc_filterData}
+        try:
+            result = xmlrpc_bindings[funct](*args)
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(xmlrpclib.dumps((result,)))
+        except IndexError:
+            pass
 
-    def filterData(self, data, contentType, params):
+    def xmlrpc_filterData(self, data, contentType, params):
         data = data.decode("utf-8", "replace")
         url = params["namespace"]
         return localnames.replace_text(data, url)
+
+    def xmlrpc_cached(self):
+        results = []
+        for url, ns in localnames.store.items():
+            preferred_name = ns["X"].get("PREFERRED-NAME", [None])[0]
+            ttl = int(ns["TIME"] + localnames.time_to_live - time.time())
+            results.append((url, preferred_name, ttl))
+        return results
+
+    def xmlrpc_dump_cache(self, url):
+        localnames.dump_cache(url)
+        return 1
+
+    def xmlrpc_validate(self, url):
+        return localnames.get_namespace(url)["ERRORS"]
+
 
 
 if __name__ == '__main__':
