9fa53023d7d02423f9fe2bee46686375ef5dacd9
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2005-01-03 15:41:05 +0000

    filtering pipe work, command line tool

---
+++ src/filteringpipe/xrfilter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python2.3
+"""
+xrfilter.py -- output result of passing stdin through an XmlRpcFilteringPipe
+
+"""
+
+import sys
+import optparse
+import xmlrpclib
+import re
+
+
+default_encoding = "text/plain; charset=utf-8"
+
+
+def main():
+    usage = "usage: %prog [options] url"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-n", "--namespace", dest="namespace",
+                      help="function namespace to use")
+    parser.add_option("-e", "--encoding", dest="encoding",
+                      default=default_encoding,
+                      help='encoding to report [defualt: "%s"]'
+                      % default_encoding)
+    parser.add_option("-a", "--argument", dest="filter_args",
+                      default=[],
+                      help="the name of an XmlRpcFilteringPipe " \
+                      "argument, followed by its string value",
+                      action="append", type="string", nargs=2)
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.error("incorrect number of arguments")
+    
+    data = sys.stdin.read()
+    filter_args = {}
+    for key, val in options.filter_args:
+        filter_args[key] = val
+    proxy = xmlrpclib.ServerProxy(args[0])
+    function_name = "filterData"
+    if options.namespace:
+        function_name = "%s.%s" % (options.namespace, function_name)
+    print getattr(proxy,function_name)(data, options.encoding, filter_args)
+
+if __name__ == "__main__":
+    main()
+
