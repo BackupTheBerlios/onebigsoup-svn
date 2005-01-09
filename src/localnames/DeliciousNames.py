eebefc912c81e955c5e664fca3077e3b852b43c1
bpt <bpt@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2005-01-09 06:07:59 +0000

    Prompt for username/password, and reduce script to ten lines :)

--- src/localnames/DeliciousNames.py
+++ src/localnames/DeliciousNames.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 # DeliciousNames: create a LocalNames namespace from your del.icio.us
 # bookmarks
-# To use: edit 'username' and 'password' strings, and run as a script.
 import xml.parsers.expat, urllib2
 
 authinfo = urllib2.HTTPBasicAuthHandler()
-authinfo.add_password('del.icio.us API', 'http://del.icio.us', 'username', 'password')
+authinfo.add_password('del.icio.us API', 'http://del.icio.us', raw_input("Username: "), raw_input("Password: "))
 urllib2.install_opener(urllib2.build_opener(authinfo))
 
 posts = []
-def hst(name, attrs):
-    if name == "post": posts.append(attrs.get('href', ''), attrs.get('extended', '')
+def hst(n,a): (n == "post") and (posts.append((a.get('href',''),a.get('extended',''))))
 
-p = xml.parsers.expat.ParserCreate()
-p.StartElementHandler = hst
-p.Parse(urllib2.urlopen("http://del.icio.us/api/posts/all").read())
+parser = xml.parsers.expat.ParserCreate(); parser.StartElementHandler = hst
+parser.Parse(urllib2.urlopen("http://del.icio.us/api/posts/all").read())
 
 print "X VERSION 1.1"
-for post in posts: if post[1] != '': print 'LN "%s" "%s"' % (post[1], post[0])
+for (n,u) in filter(lambda p: p[1]!="", posts): print 'LN "%s" "%s"' % (n,u)
