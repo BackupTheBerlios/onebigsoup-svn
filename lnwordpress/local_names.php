5cae23e7d8005e401bd26a54e1be94694cbc9c72
lionkimbro <lionkimbro@05e3fb00-29db-0310-96e5-94459cc9c8f5>
2006-08-27 14:10:14 +0000

    Local Names WordPress plugin

---
+++ lnwordpress/local_names.php
@@ -0,0 +1,199 @@
+<?php
+/*
+Plugin Name: Local Names
+Plugin URI: http://ln.taoriver.net/
+Description: A plugin that replaces text, when you submit content. Look under "Options" to customize.
+Author: Lion Kimbro
+Version: 1.0
+Author URI: http://lion.taoriver.net/
+*/
+
+/*  Local Names WordPress Plugin, automatically binds names to URLs.
+    Copyright (C) 2006  Lion Kimbro  (lion@speakeasy.org)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Lion Kimbro (lion@speakeasy.org)
+    15001 35th Ave W Apt 5-203
+    Lynnwood, WA 98087-2373
+*/
+
+require_once(ABSPATH . WPINC . '/class-IXR.php');
+
+function array_to_string($array) {
+	foreach ( $array as $index => $val ) {
+		$val2 .= $index;
+		$val2 .= ', ';
+		$val2 .= $val;
+		$val2 .= '; ';
+	}
+	return $val2;
+}
+
+function array_of_arrays_to_string($aoa) {
+	$ret = '[';
+	foreach ( $aoa as $index => $array ) {
+		$ret .= '[';
+		foreach ($array as $key => $val) {
+			$ret .= $val;
+			$ret .= ',';
+		}
+	$ret .= ']';
+	}
+	
+	$ret .= ']';
+	
+	return $ret;
+}
+
+// Return an array of URLs, given the results of a LNXRQueryI find_many.
+// For those lookups that failed, return just: 'lookup_failed'.
+function process_find_many_results($results) {
+	$out = array();
+	foreach ( $results as $index => $error_and_string ) {
+		if ( 0 == $error_and_string[0] ) {
+			$out[$index] = $error_and_string[1];
+		} else {
+			$out[$index] = 'lookup_failed';
+		}
+	}
+
+	return $out;
+}
+
+class LKStringFeeder {
+	public function __construct($inStrings) {
+		$this->feed = $inStrings;
+	}
+	function next_result() {
+		return array_shift($this->feed);
+	}
+	function replace_w_link($matches) {
+		$url = $this->next_result();
+		$text = $matches[1];  // just the portion [[inside]]
+		if ( isset($matches[2]) and ($matches[2] != "") )
+		{
+			$text = substr($matches[2], 1);  // [[link | text]]
+		}
+		if ( 'lookup_failed' == $url ) {
+			return sprintf('<font color="red">%s</font>', $text);
+		} else {
+			return sprintf('<a href="%s">%s</a>', $url, $text);
+		}
+	}
+}
+
+function lk_ensure_options_set() {
+	add_option('local_names', array('LNXRQI' => 'http://taoriver.net:8123/', 'ns_url' => 'http://ln.taoriver.net/servicenames.txt'), 'Local Names options -- LNXRQueryI, and Namespace URL');
+	return get_option('local_names');
+}
+
+function lk_update_option_values($LNXRQI, $ns_url) {
+	update_option('local_names', array('LNXRQI' => $LNXRQI, 'ns_url' => $ns_url));
+	return get_option('local_names');
+}
+
+function lk_change_content($content) {
+	$option_values = lk_ensure_options_set();
+	// $pattern = '/\\[\\[([^]]+)\\]\\]/i';
+        $pattern = '/\\[\\[([^\\]|]+)(|[^\\]]+)?\\]\\]/i';
+	preg_match_all($pattern, $content, $out, PREG_PATTERN_ORDER);
+	$matches = $out[1];  // pick the interiors, not the whole expresions
+	if ( count($matches) == 0 ) {
+		return $content;
+	}
+	foreach ( $matches as $index => $name ) {
+		$matches[$index] = split(':', $name);
+	}
+	$client = new IXR_Client($option_values['LNXRQI']);
+	if ( $client->query('lnquery.find_many', $option_values['ns_url'], $matches, 'LN', 'default') == FALSE )
+	{
+                $err_msg = '<p><font color="red">';
+                $err_msg .= 'Hm; I can\\\'t seem to connect to the LNXRQueryI at ';
+                $err_msg .= '<a href="' . $option_values['LNXRQI'] . '">' . $option_values['LNXRQI'] . '</a></font></p>';
+                $err_msg .= '<p><font color="red">client-IXR.php reports: <b>' . $client->error->message . '</b></font></p>';
+                return $err_msg . $content;
+	}
+	$results = process_find_many_results($client->getResponse());
+	$content = preg_replace_callback($pattern, array(new LKStringFeeder($results), 'replace_w_link'), $content);
+	
+	// DEBUGGING:
+	// $content = $content . array_to_string($results);
+	
+	return $content;
+}
+
+function ln_options_subpanel() {
+	$option_values = lk_ensure_options_set();
+	if ( isset($_POST['info_update']) ) {
+		if ( isset($_POST['LNXRQI']) and isset($_POST['ns_url']) ) {
+			$option_values = lk_update_option_values($_POST['LNXRQI'], $_POST['ns_url']);
+			?>
+                        <div class="updated">
+                        <p>Local Names XML-RPC Query Interface URL set to: <b><?php _e($option_values['LNXRQI']); ?></b></p>
+                        <p>Local Names namespace URL set to: <b><?php _e($option_values['ns_url']); ?></b></p>
+                        </div>
+                        <?php
+			$client = new IXR_Client($option_values['LNXRQI']);
+
+			if ( TRUE == $client->query('lnquery.get_server_info') ) {
+				$response = $client->getResponse();
+				if ( 'OBS-LNQS' == $response['IMPLEMENTATION'] ) {
+					?><div class="updated"><p>Connection confirmed- You're ready to go!</p></div><?php
+				} else {
+					?><div class="error"><p>Hmm; I can connect to a Server, but it's not responding to lnquery.get_server_info() like I expect.</p></div><?php
+				}
+			} else {
+				?><div class="error"><p>Cannot Connect to Name Server- <?php _e($client->error->message); ?></p></div><?php
+			}
+		} else {
+			?>
+			<div class="error"><p>ERROR: Missing either (or both) of: LNXRQI or ns_url</p></div>
+			<?php
+		}
+	} ?>
+<div class=wrap>
+  <form method="post">
+    <h2>Local Names Plugin</h2>
+     <fieldset name="set1">
+	<p><b>LNXRQueryI URL:</b> <input type="text" name="LNXRQI" size="63" value="<?php _e($option_values['LNXRQI'])?>"/></p>
+        <p>"LNXRQueryI" refers to the "Query" server- a computer that takes care of figuring out what URL to bind your names to.</p>
+        <p>If you don't know, see if one of these work:</p>
+        <ul><li><code>http://ln.taoriver.net/lnxrqs</code></li><li><code>http://taoriver.net:8123/</code></li></ul>
+        <p>That refers to two servers running on Lion's computer. They work, or at least they do today: 2006-08-25.</p>
+	<p><b>Namespace URL:</b> <input type="text" name="ns_url" size="63" value="<?php _e($option_values['ns_url'])?>"/></p>
+        <p>This refers to the URL of your personal namespace. (Or somebody else's namespage, that you want to make use of.)
+           The namespace is what binds individual names to URLs.</p>
+        <p>You could try using Lion's: <code>http://taoriver.net:9000/description?namespace=lion</code></p>
+        <p>...but I think you'll have much more fun, if you get one of your own, at <a href="http://taoriver.net:9000/">My Local Names.</a></p>
+     </fieldset>
+<div class="submit">
+  <input type="submit" name="info_update" value="<?php
+	_e('Update options', 'Localization name')
+	?>" /></div>
+  </form>
+ </div><?php
+}
+
+function lk_options_page() {
+	if ( function_exists('add_options_page') ) {
+		add_options_page('lnsubpaneltitle', 'Local Names', 10, basename(__FILE__), 'ln_options_subpanel');
+	}
+}
+
+add_filter('content_save_pre', 'lk_change_content');
+add_action('admin_menu', 'lk_options_page');
+
+?>
---
+++ lnwordpress/readme.txt
@@ -0,0 +1,182 @@
+=== Local Names ===
+
+Tags: localnames, linking, link, links, hyperlinking, hyperlink, hyperlinks, name, names, urls, url
+Contributors: LionKimbro
+
+Local Names is a plugin to automatically link names to URL.
+
+
+== Thank you! ==
+
+						       	|___
+  Thank you for trying out Local Names!		       /   /
+				       	       	      /	\ /
+  I think you'll find Local Names to be very		 /.--.
+  useful, and indispensible once you've used	       	/ |  |
+  it for a while.				       /  '--'
+
+  Let me know what you think, and let me           Chinese Character
+  know if you need any help setting it up.           for "Name"
+
+  You can just call me on my cell phone, or
+  by the information at the bottom of this
+  page, if you're uncomfortable calling me.
+
+  Thank you,
+    Lion Kimbro
+    206.427.2545
+
+
+== Installation ==
+
+
+  SUMMARY:
+  1. Copy local_names.php to plugins directory.
+  2. Activate plugin.
+  3. Configure in Options, Local Names.
+     4. Enter LNXRQueryI URL.
+     5. Enter Namespace Description URL.
+     6. Click "Update Options."
+  7. You're done!
+
+
+1. Copy local_names.php to your $blog/wp-content/plugins directory.
+2. Log into your blogs "Site Admin," click on "Plugins," and then
+   activate the "Local Names" plugin.
+3. Click on "Options" and then "Local Names."
+4. Enter your Local Names XML-RPC Query Interface URL.
+
+    If you don't know what to put here, just put either:
+
+      http://ln.taoriver.net/lnxrqs
+
+    ... or, if that doesn't work, ...
+
+      http://taoriver.net:8123/
+
+    These are hosted on Lion Kimbro's computer today, 2006-08-26.
+
+5. Enter your Namespace Description URL.
+
+    Your **namespace** is your **personal** collection of names for
+    things on the Internet.  If you call "http://wordpress.org/" simply
+    "WordPress," then that goes into your namespace.
+
+    Your namespace is written on a page on the Internet; That page has a
+    URL, and that URL is known as your "Namespace Description URL."
+
+    There are two ways to make your namespace description.  One is to
+    write it manually, by hand, and put it on the Internet.  It's URL is
+    your namespace description URL.
+
+    An easier way is to make a namespace on "My Local Names."
+
+    Visit:
+
+        http://taoriver.net:9000/
+
+    ...and type in your name, and a password if you want one.  It'll
+    give you your Namespace Description URL, and a bookmarklet you can
+    use to add names to your Namespace Description.
+
+6. Click "Update options."
+7. Your installation is now complete.
+
+
+  SUMMARY:
+  1. Copy local_names.php to plugins directory.
+  2. Activate plugin.
+  3. Configure in Options, Local Names.
+     4. Enter LNXRQueryI URL.
+     5. Enter Namespace Description URL.
+     6. Click "Update Options."
+  7. You're done!
+
+
+== How to Use ==
+
+
+It's really easy!
+
+When you write a post, use [[brackets]] to automatically link names.
+
+Here is an example post:
+
+ [--B-I-ABC--*-123-<->--[-I-]--oo-o><o-&-H--)-(-HTML-?----------------]
+ |								      |
+ |  Good morning! This is [[Lion Kimbro]] making use of the	      |
+ |  [[Local Names Plugin]] to make this post.  It helps me automate   |
+ |  [[hyperlink]]ing.  This way, I don't have to look up [[URL]]s     |
+ |  for every page I might want to link to.			      |
+ |								      |
+ |  Something you might not know about [[Local Names]]:		      |
+ |								      |
+ |  Did you know that you can configure [[FireFox]] to use Local      |
+ |  Names to jump to web pages?  If I type "ln Local Names" in my     |
+ |  browser address bar, it automatically takes me to		      |
+ |  "http://ln.taoriver.net/".  And if I type "ln pancakes", it	      |
+ |  automatically takes me to					      |
+ |  "http://youtube.com/watch?v=PnCVZozHTG8", the [[Pancakes]] video  |
+ |  on [[YouTube]].						      |
+ |								      |
+  --------------------------------------------------------------------
+
+WHEN YOU TELL IT to Save, or when you tell it to "Save and Continue
+Editing," the Local Names plugin will hyperlink all the bracketed text.
+
+You can also hyperlink without using the name:
+
+ [--B-I-ABC--*-123-<->--[-I-]--oo-o><o-&-H--)-(-HTML-?----------------]
+ |							       	      ^
+ |  I was talking with [[Joel Ford | my friend]] the other day,	      |
+ |  about [[StarCraft | a game we used to play a lot.]]  I told him   |
+ |  that [[Sakura | my daughter]] has been playing it a lot lately,   |
+ |  and is getting pretty good at the [[Zerg]] missions.	      |
+ |								      v
+  --------------------------------------------------------------------
+
+Again: hyperlinking happens WHEN YOU HIT "SAVE" OR "SAVE AND CONTINUE
+EDITING."
+
+
+== What if there's a problem? ==
+
+
+If you can't connect to the Internet, or
+if the nameserver is down, you'll get a	         *
+message in red, at the top of your post,        _|             __
+telling you what is wrong.		       (__  Question?   _)
+                                                               |
+If you use a name, but it's not defined,                       *
+that name will be written in red, rather       	       	       	   jgs
+than hyperlinked.
+
+If you define a new name, but the cache hasn't expired, go to the
+cache-dumping website:
+
+  http://ln.taoriver.net/dumpcache
+
+If you have any other questions, don't hesitate to e-mail or call.
+
+
+== Enjoy! ==
+
+
+  I **really** hope you like Local Names, and welcome ALL feedback!
+
+  Just writing a blog post, telling what you think, is really helpful.
+  And you can always email me (lion@speakeasy.org,) or even give me a
+  phone call, at 206.427.2545.
+
+  I appreciate any and all ideas or advice. Even criticism is
+  appreciated.
+
+  So again, one last time:
+
+  Thank you!
+
+
+== Screenshots ==
+
+1. This is a picture showing how to link local names with the WordPress Plugin.
+
